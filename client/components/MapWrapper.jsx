71ec4f40d069bb95fd032f5ce3155a505cf399a9
Sarah's Mac <sarahsongg@yahoo.com>
2020-08-23 19:51:41 -0700

    availability with dynamic name change

--- client/components/MapWrapper.jsx
+++ client/components/MapWrapper.jsx
@@ -6,7 +6,7 @@ const MapWrapper = props => {
     //array of objects with name, trims, name of trims (id), lat, long, id of dealership location
     //selected dealer is automatically on brooklyn
 const [trimData, setTrimData] = useState([]);
-const [selectedDealer, setSelectedDealer] = useState(1); 
+const [selectedDealer, setSelectedDealer] = useState(0); 
 //mock data
 /* **Location 1:** A dealership in Brooklyn, NY - All 3 trims are available
 	- **Location 2:** A dealership in Queens, NY - LS and LS HYBRID
@@ -15,14 +15,13 @@ const [selectedDealer, setSelectedDealer] = useState(1);
     */
    //
 
-   const trims = {1: ['LS HYBRID', 'LS F SPORT', 'LS'], 2 : ['LS HYBRID', 'LS F SPORT', 'LS'], 3: ['LS F SPORT'], 4: [] }
-const dealerships = [{name: 'Brooklyn, NY', lat: 40.6782, long: -73.9442, id: 1}, {name: 'Queens, NY', lat: 40.7282, long: -73.7949, id: 2}, {name: 'Newark, NJ', lat: 40.7357, long: -74.1724, id: 3}, {name: 'Plainfield, NJ', lat: 40.6337, long: -74.4074, id: 4}]
+   const trims = {0: ['LS HYBRID', 'LS F SPORT', 'LS'], 1 : ['LS HYBRID', 'LS F SPORT', 'LS'], 2: ['LS F SPORT'], 3: [] }
+const dealerships = [{name: 'Brooklyn, NY', lat: 40.6782, long: -73.9442, id: 0}, {name: 'Queens, NY', lat: 40.7282, long: -73.7949, id: 1}, {name: 'Newark, NJ', lat: 40.7357, long: -74.1724, id: 2}, {name: 'Plainfield, NJ', lat: 40.6337, long: -74.4074, id: 3}]
 
 return(
-<div>
+<div className='map-wrapper'>
     <MapDisplay dealerships={dealerships} setSelectedDealer={setSelectedDealer}/>
-INSIDE MAP WRAPPER
-    <TrimAvailability selectedDealer={selectedDealer} trims={trims} />
+    <TrimAvailability selectedDealer={selectedDealer} trims={trims} dealerships={dealerships}/>
 </div>
   )  
 }
--- client/components/TrimAvailability.jsx
+++ client/components/TrimAvailability.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const TrimAvailability = ({selectedDealer, trims}) => {
+const Wrapper = styled.div`
+width: 100%;
+height: 70vh; 
+background-color: pink;
+`;
+
+
+const TrimAvailability = ({selectedDealer, trims, dealerships}) => {
 
     return (
-            <div> 
+            <div className='trim-availability'> 
+            <Wrapper>
+                <h2> Availability for {dealerships[selectedDealer].name}</h2>
             {trims[selectedDealer]}
+            </Wrapper>
             </div>
     )
 }
