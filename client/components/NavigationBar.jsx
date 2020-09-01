20ffe75fab9c9244b39c20a902c13ec144971886
Sarah's Mac <sarahsongg@yahoo.com>
2020-09-01 11:47:26 -0700

    final styling of mobile and web view

--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -1,16 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/Lexus.png';
 import User from './User';
-// import Menu from './Menu';
-
 // import { BrowserRouter as Router, Link } from 'react-router-dom';
 //i'd use router but i'm setting up navigation bar with a tags for this case
-const NavigationBar = () => {
-    const [toggleOpen, setToggleOpen] = useState(false);
 
-    const menuIconDisplay = toggleOpen ? (
-    <div><li className="nav-item">
-          <a href="/">HOME</a>
+const NavigationBar = () => {
+  const [toggleOpen, setToggleOpen] = useState(false);
+
+  const menuIconDisplay = toggleOpen ? (
+    <div className="menu-toggle-list">
+      <div className="menu-toggle-item">
+        <a href="/">HOME</a>
+      </div>
+      <div className="menu-toggle-item">
+        <a href="/about">ABOUT</a>
+      </div>
+      <div className="menu-toggle-item">
+        <a href="/contact">CONTACT</a>
+      </div>
+      <div className="menu-toggle-item">
+        <a href="#">MY ACCOUNT</a>
+      </div>
+    </div>
+  ) : null;
+
+  //hide the navbar and call the above menu-items
+  return (
+    <div>
+      <header className="menu-container">
+        <div>
+          <img
+            src={logo}
+            alt="lexus logo"
+            height="75vh"
+            className="lexus-logo"
+          />
+          <a
+            href="#"
+            className="toggle-button"
+            onClick={() => setToggleOpen(!toggleOpen)}
+          >
+            <span className="bar" />
+            <span className="bar" />
+            <span className="bar" />
+          </a>
+        </div>
+        <div>{menuIconDisplay}</div>
+      </header>
+      <header className="nav-container">
+        <ul className="nav-bar">
+          <li className="nav-item">
+            <img
+              src={logo}
+              alt="lexus logo"
+              height="75vh"
+              className="lexus-logo"
+            />
+          </li>
+
+          <li className="nav-item">
+            <a href="/">HOME</a>
           </li>
           <li className="nav-item">
             <a href="/about">ABOUT</a>
@@ -18,45 +67,11 @@ const NavigationBar = () => {
           <li className="nav-item">
             <a href="/contact">CONTACT</a>
           </li>
-          <li className="nav-item-account">
-            <a href="#">MY ACCOUNT</a>
-          </li></div>
-    ) : (
-      null
-    );
-  
-//hide the navbar and call the above menu-items
-  return (
-    <header className="nav-container">
-        <a href="#" className="toggle-button" onClick={()=> setToggleOpen(!toggleOpen)}>
-        <span className="bar" />
-        <span className="bar" />
-        <span className="bar" />
-        </a>
-        {menuIconDisplay}
-      <ul className="nav-bar">
-        <li className="nav-item">
-          <img
-            src={logo}
-            alt="lexus logo"
-            height="75vh"
-            className="lexus-logo"
-          />
-        </li>
+        </ul>
 
-        <li className="nav-item">
-          <a href="/">HOME</a>
-        </li>
-        <li className="nav-item">
-          <a href="/about">ABOUT</a>
-        </li>
-        <li className="nav-item">
-          <a href="/contact">CONTACT</a>
-        </li>
-      </ul>
-
-      <User />
-    </header>
+        <User />
+      </header>
+    </div>
   );
 };
 
